0f8b4d0103065a8ecc99dcb731304a83265774d2
Ferdy Lim <ferdy.lim@springkraf.com>
2024-07-17 23:45:35 +0700

    Revert "day 9 programming"
    This reverts commit 038011df7b0fe49f266906df7d770498cd074a2b.

--- 158-a.py
+++
@@ -1,7 +0,0 @@
-# https://codeforces.com/contest/158/problem/A
-
-n,k = [int(x) for x in input().split(' ')]
-arr = [int(x) for x in input().split(' ')]
-arr.sort(reverse=True)
-filtered = filter(lambda x: x >=arr[k-1] and x != 0, arr)
-print(len(list(filtered)))
--- 50-a.py
+++
@@ -1,5 +0,0 @@
-# https://codeforces.com/contest/50/problem/A
-
-m,n = [int(x) for x in input().split(' ')]
-
-print((m*n)//2)
